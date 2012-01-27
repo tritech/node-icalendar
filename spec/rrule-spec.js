98c1a7f6f1bb5378e7b1604e48d0577974f7863b
James Emerton <james@tri-tech.com>
2012-01-27 15:16:48 -0800

    Refactored RRULE to add string ctor, rename nextOccurs() to next()

--- spec/rrule-spec.js
+++ spec/rrule-spec.js
@@ -12,17 +12,17 @@ describe("RRule", function() {
         var rrule = new RRule(RRule.parse('FREQ=YEARLY;BYMONTH=3;BYDAY=1SU'), start);
         var rrule5 = new RRule(RRule.parse('FREQ=YEARLY;INTERVAL=5;BYMONTH=3;BYDAY=1SU'), start);
 
-        expect(rrule.nextOccurs(new Date(2011,1,4)))
+        expect(rrule.next(new Date(2011,1,4)))
                 .toEqual(new Date(2011,2,6,2,0,0));
 
-        expect(rrule.nextOccurs(new Date(2011,3,7)))
+        expect(rrule.next(new Date(2011,3,7)))
                 .toEqual(new Date(2012,2,4,2,0,0));
 
-        expect(rrule.nextOccurs(new Date(2011,2,6,2,0,0)))
+        expect(rrule.next(new Date(2011,2,6,2,0,0)))
                 .toEqual(new Date(2012,2,4,2,0,0));
 
 
-        expect(rrule5.nextOccurs(new Date(2011,1,4)))
+        expect(rrule5.next(new Date(2011,1,4)))
                 .toEqual(new Date(2015,2,1,2,0,0));
     });
 
@@ -30,10 +30,10 @@ describe("RRule", function() {
         var start = new Date(2011,0,1,2,0,0);
         var rrule = new RRule(RRule.parse('FREQ=MONTHLY;BYDAY=1SU;UNTIL=20110201'), start);
 
-        expect(rrule.nextOccurs(new Date(2011,0,1,2,0,0)))
+        expect(rrule.next(new Date(2011,0,1,2,0,0)))
                 .toEqual(new Date(2011,0,2,2,0,0));
 
-        expect(rrule.nextOccurs(new Date(2011,0,2,2,0,0)))
+        expect(rrule.next(new Date(2011,0,2,2,0,0)))
                 .toEqual(null);
     });
 
@@ -41,10 +41,10 @@ describe("RRule", function() {
         var start = new Date(2011,0,1,2,0,0);
         var rrule = new RRule(RRule.parse('FREQ=MONTHLY;BYDAY=1SU'), start);
 
-        expect(rrule.nextOccurs(new Date(2011,0,1,2,0,0)))
+        expect(rrule.next(new Date(2011,0,1,2,0,0)))
                 .toEqual(new Date(2011,0,2,2,0,0));
 
-        expect(rrule.nextOccurs(new Date(2011,0,2,2,0,0)))
+        expect(rrule.next(new Date(2011,0,2,2,0,0)))
                 .toEqual(new Date(2011,1,6,2,0,0));
     });
 });
